1f61c793512383f940633d6b8a24ed2193569509
Carlos Mena <carlos.mena@applydigital.com>
2023-02-23 22:01:13 -0300

    feat: first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,11 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=mKOeLPZQXeVcJEzRt1ZQyFQ0TV5E3zNq&q=${category}&limit=20`
+    const resp = await fetch(url)
+    const { data } = await resp.json()
+
+    return data.map((img) => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url,
+    }))
+}
